43eeb741a5af15a19ef256d7cc3c30cd594a9d60
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-11-29 13:20:29 +0100

    UserRound is made

--- src/app/Shared/Round.model.ts
+++ src/app/Shared/Round.model.ts
@@ -1,10 +1,13 @@
 import {Tournament} from './Tournament.model';
 import {Match} from './Match.model';
+import {UserRound} from './UserRound.model';
 
 export interface Round {
   id: number;
   roundNumber: number;
   tournament?: Tournament;
+  tournamentId?: number;
   totalGoals: number;
   matches?: Match[];
+  roundPoints?: UserRound;
 }
---
+++ src/app/Shared/UserRound.model.ts
@@ -0,0 +1,12 @@
+import {User} from './User.model';
+import {Round} from './Round.model';
+
+
+export interface UserRound {
+  id: number;
+  user: User;
+  userId: number;
+  round: Round;
+  roundId: number;
+  userPoints: number;
+}
