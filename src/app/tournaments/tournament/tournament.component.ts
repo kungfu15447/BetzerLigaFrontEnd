07928e5b9eac3fc5ccd458b2b9e61db42b7aa69d
milimolo <bondemand@hotmail.dk>
2019-12-10 11:08:42 +0100

    addRoundwithoutDeadline

--- src/app/tournaments/tournament/tournament.component.ts
+++ src/app/tournaments/tournament/tournament.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Tournament} from '../../Shared/Tournament.model';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {TournamentService} from '../shared/tournament.service';
+import {Round} from '../../Shared/Round.model';
+import {RoundService} from '../../rounds/shared/round.service';
+import {totalmem} from 'os';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-tournament',
@@ -11,12 +15,27 @@ import {TournamentService} from '../shared/tournament.service';
 export class TournamentComponent implements OnInit {
   tournament: Tournament;
   loading: boolean;
+  submitted = false;
+  round: any = {};
+  roundForm = new FormGroup({
+    roundNumber: new FormControl(''),
+    totalGoals: new FormControl(''),
+    tournamentId: new FormControl(''),
+    tournament: new FormControl('')
+  });
+
   constructor(private route: ActivatedRoute,
-              private tourService: TournamentService
-  ) { }
+              private tourService: TournamentService,
+              private roundService: RoundService
+  ) {
+  }
 
   ngOnInit() {
     this.getTour();
+  }
+
+  get roundNumber() {
+    return this.roundForm.get('roundNumber');
   }
 
   getTour(): void {
@@ -25,6 +44,20 @@ export class TournamentComponent implements OnInit {
     this.tourService.getTour(id)
       .subscribe(tournament => this.tournament = tournament);
     this.loading = false;
+  }
+
+  addRound(): void {
+    this.submitted = true;
+    const currentTournament = this.tournament;
+    const roundFromFields = this.roundForm.value;
+    roundFromFields.totalGoals = 0;
+    roundFromFields.tournamentId = currentTournament.id;
+    if (currentTournament.rounds.length < currentTournament.numberOfRounds || roundFromFields.roundNumber !== 0) {
+      this.roundService.addRound(roundFromFields).subscribe(
+        roundo => currentTournament.rounds.push(roundo)
+      );
+    } else {
+    }
   }
 
 }
